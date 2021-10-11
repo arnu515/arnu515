b7ff3be165a65ac534fd614656526d19cc25c68b
arnu515 <arnu5152@gmail.com>
2021-10-11 21:12:40 +0530

    feat: add tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,28 @@
-import {GeistProvider,CssBaseline} from "@geist-ui/react"
+import { GeistProvider, CssBaseline } from "@geist-ui/react";
+import { useEffect } from "react";
+import "tailwindcss/tailwind.css";
 
-export default function Application({Component, pageProps}) {
-  return <>
-    <GeistProvider>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </GeistProvider>
-  </>
+export default function Application({ Component, pageProps }) {
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (!theme) {
+      const dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      if (dark) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else localStorage.setItem("theme", "light");
+    } else if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else localStorage.setItem("theme", "light");
+  }, []);
+
+  return (
+    <>
+      <GeistProvider>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </GeistProvider>
+    </>
+  );
 }
