3ce6b21332c5f4431f319547e528e279826e2f82
arnu515 <arnu5152@gmail.com>
2021-10-11 20:22:46 +0530

    feat: add @geist-ui/react
    Geist UI is a react UI library based on Vercel's style guide

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import React from "react";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-);
+const Index: React.FC = () => {
+  return <>
+    <h1>Hello, world</h1>
+  </>
+}
 
-export default IndexPage;
+export default Index;
