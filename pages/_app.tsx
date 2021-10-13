54eb6fe34cab92f088220432f925be30a867ced7
arnu515 <arnu5152@gmail.com>
2021-10-13 14:06:27 +0530

    feat: add useTheme hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,7 @@
 import { GeistProvider, CssBaseline } from "@geist-ui/react";
-import { useEffect } from "react";
 import "tailwindcss/tailwind.css";
 
 export default function Application({ Component, pageProps }) {
-  useEffect(() => {
-    const theme = localStorage.getItem("theme");
-    if (!theme) {
-      const dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-      if (dark) {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-      } else localStorage.setItem("theme", "light");
-    } else if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else localStorage.setItem("theme", "light");
-  }, []);
-
   return (
     <>
       <GeistProvider>
