bf3a9c7364508f781d9644a0799eba1335c663aa
arnu515 <arnu5152@gmail.com>
2021-10-15 09:31:12 +0530

    fix: fix geist ui elements not being of the correct theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
-import { GeistProvider, CssBaseline } from "@geist-ui/react";
+import React from "react";
+import { GeistProvider, CssBaseline, GeistProviderProps } from "@geist-ui/react";
 import "./app.css";
+import useTheme, { ThemeProvider } from "../utils/hooks/useTheme";
+
+function Geist(props: React.PropsWithChildren<GeistProviderProps>) {
+  const { theme } = useTheme();
+
+  return (
+    <GeistProvider {...props} themeType={theme}>
+      {props.children}
+    </GeistProvider>
+  );
+}
 
 export default function Application({ Component, pageProps }) {
   return (
-    <>
-      <GeistProvider>
+    <ThemeProvider>
+      <Geist>
         <CssBaseline />
         <Component {...pageProps} />
-      </GeistProvider>
-    </>
+      </Geist>
+    </ThemeProvider>
   );
 }
