6abdafd944753753508f161c327b938298a59bac
arnu515 <arnu5152@gmail.com>
2021-10-27 16:47:10 +0530

    add css, react-toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 import "./app.css";
+import "./form.css";
+import "react-toastify/dist/ReactToastify.min.css";
 
 export default function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
 }
