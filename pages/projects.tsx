3ce9039c8f01f8628f90d63be09a6a91b84c27a6
arnu515 <arnu5152@gmail.com>
2021-10-29 12:11:37 +0530

    fix: logos were too large
    Logos of languages were too large

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -8,7 +8,7 @@ const ProjectCardLanguageIcon: React.FC<{ language: Project["language"] }> = ({
   switch (language) {
     case "python":
       return (
-        <svg viewBox="0 0 128 128">
+        <svg viewBox="0 0 128 128" width="24" height="24">
           <linearGradient
             id="python-original-a"
             gradientUnits="userSpaceOnUse"
@@ -63,7 +63,7 @@ const ProjectCardLanguageIcon: React.FC<{ language: Project["language"] }> = ({
       );
     case "javascript":
       return (
-        <svg viewBox="0 0 128 128">
+        <svg viewBox="0 0 128 128" width="24" height="24">
           <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185H1.408z"></path>
           <path
             fill="#323330"
@@ -73,7 +73,7 @@ const ProjectCardLanguageIcon: React.FC<{ language: Project["language"] }> = ({
       );
     case "java":
       return (
-        <svg viewBox="0 0 128 128">
+        <svg viewBox="0 0 128 128" width="24" height="24">
           <path
             fill="#0074BD"
             d="M47.617 98.12s-4.767 2.774 3.397 3.71c9.892 1.13 14.947.968 25.845-1.092 0 0 2.871 1.795 6.873 3.351-24.439 10.47-55.308-.607-36.115-5.969zm-2.988-13.665s-5.348 3.959 2.823 4.805c10.567 1.091 18.91 1.18 33.354-1.6 0 0 1.993 2.025 5.132 3.131-29.542 8.64-62.446.68-41.309-6.336z"
