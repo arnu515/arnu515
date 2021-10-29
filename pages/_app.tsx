cfedc0cb67c9b0aa6dfe9030132177933acb7c3a
arnu515 <arnu5152@gmail.com>
2021-10-29 11:19:46 +0530

    feat: add sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,17 @@ import { ToastContainer } from "react-toastify";
 import "./app.css";
 import "./form.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import Sidenav from "../lib/components/Sidenav";
 
 export default function Application({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <div>
+        <Sidenav />
+        <div className="ml-16">
+          <Component {...pageProps} />
+        </div>
+      </div>
       <ToastContainer />
     </>
   );
