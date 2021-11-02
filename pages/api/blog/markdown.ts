94227438c6377f35c579bff412e67fd19e4f8086
arnu515 <arnu5152@gmail.com>
2021-11-02 15:46:30 +0530

    feat: add API handler to parse markdown to HTML

---
+++ pages/api/blog/markdown.ts
@@ -0,0 +1,32 @@
+import { NextApiHandler } from "next";
+import MarkdownIt from "markdown-it";
+import hljs from "highlight.js";
+
+const handler: NextApiHandler = (req, res) => {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method not allowed" });
+    return;
+  }
+
+  const { content } = req.body;
+
+  const mdit = new MarkdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+    highlight: (str, lang) => {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(lang, str).value;
+        } catch (__) {}
+      }
+      return ""; // use external default escaping
+    }
+  });
+  mdit.use(require("markdown-it-anchor"));
+  mdit.use(require("markdown-it-attrs"));
+
+  res.status(200).json({ content: mdit.render(content) });
+};
+
+export default handler;
