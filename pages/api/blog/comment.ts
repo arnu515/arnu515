d7b5795403e5795d950900f6dfc2e85684f4e51e
arnu515 <arnu5152@gmail.com>
2021-11-15 16:56:39 +0530

    feat: send discord webhook message on new comment

--- pages/api/blog/comment.ts
+++ pages/api/blog/comment.ts
@@ -16,7 +16,7 @@ const handler: NextApiHandler = async (req, res) => {
   const { error } = joi
     .object({
       slug: joi.string().required(),
-      content: joi.string().required().max(500).trim()
+      content: joi.string().required().max(500).trim(),
     })
     .validate(req.body);
   if (error) {
@@ -28,7 +28,7 @@ const handler: NextApiHandler = async (req, res) => {
     data: {
       content,
       postId: slug,
-      userId: session.userId
+      userId: session.userId,
     },
     include: {
       user: {
@@ -37,13 +37,27 @@ const handler: NextApiHandler = async (req, res) => {
           avatar: true,
           profile: {
             select: {
-              full_name: true
-            }
-          }
-        }
-      }
-    }
+              full_name: true,
+            },
+          },
+        },
+      },
+    },
   });
+
+  // Send message using discord webhook
+  const res2 = await fetch(process.env.DISCORD_WEBHOOK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: `@everyone New comment on post \`${slug}\`.\n\nContent: ${content}`,
+    }),
+  });
+  if (!res2.ok) {
+    console.error("Failed to send discord webhook", await res2.json());
+  }
   res.status(200).json({ comment });
 };
 
